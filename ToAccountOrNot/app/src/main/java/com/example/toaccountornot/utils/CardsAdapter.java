31304108a2589c7e96d7a4e48e1ac2b648f8d2b8
jyow <49602630+caskr@users.noreply.github.com>
2020-10-27 00:33:08 +0800

    存档

--- ToAccountOrNot/app/src/main/java/com/example/toaccountornot/utils/CardsAdapter.java
+++ ToAccountOrNot/app/src/main/java/com/example/toaccountornot/utils/CardsAdapter.java
@@ -4,43 +4,69 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.toaccountornot.R;
 
 import java.util.List;
 
-public class CardsAdapter extends ArrayAdapter {
+public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.ViewHolder>{
 
-    private int CardId;
-    public CardsAdapter(Context context, int headImage, List<Cards> obj){
-        super(context,headImage,obj);
-        CardId = headImage;
+    private Context mContext;
+    private List<Cards> mcardlist;
+
+    public CardsAdapter(Context context, List<Cards> cardlist){
+        mContext = context;
+        mcardlist = cardlist;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
+        TextView remark;
+        ImageView headImage;
+        ImageView card_return;
+        TextView in;
+        TextView out;
+        TextView sur;
+
+        public ViewHolder(View view) {
+            super(view);
+            TextView name = view.findViewById(R.id.account_card);
+            TextView remark = view.findViewById(R.id.account_remark);
+            ImageView headImage = view.findViewById(R.id.account_image);
+            ImageView card_return = view.findViewById(R.id.card_return);
+            TextView in = view.findViewById(R.id.account_in);
+            TextView out = view.findViewById(R.id.account_out);
+            TextView sur = view.findViewById(R.id.account_sur);
+        }
     }
 
     @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
-        Cards card = (Cards) getItem(position);
-        View view = LayoutInflater.from(getContext()).inflate(CardId,parent,false);//这个是实例化一个我们自己写的界面Item
-        TextView name = view.findViewById(R.id.account_card);
-        TextView remark = view.findViewById(R.id.account_remark);
-        ImageView headImage = view.findViewById(R.id.account_image);
-        ImageView card_return = view.findViewById(R.id.card_return);
-        TextView in = view.findViewById(R.id.account_in);
-        TextView out = view.findViewById(R.id.account_out);
-        TextView sur = view.findViewById(R.id.account_sur);
-        name.setText(card.getCard());
-        remark.setText(card.getRemark());
-        headImage.setImageResource(card.getCardid());
-        card_return.setImageResource(R.drawable.returnsignal);
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View view = LayoutInflater.from(parent.getContext().inflate(R.layout.cards_list_item,parent,false);
+        final ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Cards card = mcardlist.get(position);
+        holder.name.setText(card.getCard());
+        holder.remark.setText(card.getRemark());
+        holder.headImage.setImageResource(card.getCardid());
+        holder.card_return.setImageResource(R.drawable.returnsignal);
         String income = "+"+ card.getIncome();
-        in.setText(income);
+        holder.in.setText(income);
         String outcome  = "-"+card.getOutcome();
-        out.setText(outcome);
+        holder.out.setText(outcome);
         String surplus = card.getSurplus() >0 ? "+"+card.getSurplus():""+card.getSurplus();
-        sur.setText(surplus);
-        return view;
+        holder.sur.setText(surplus);
+    }
+    @Override
+    public int getItemCount() {
+        return mcardlist.size();
     }
 }
