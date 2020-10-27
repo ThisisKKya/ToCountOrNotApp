7d613606a592cbc3fc9558eea84092b903d56973
jyow <49602630+caskr@users.noreply.github.com>
2020-10-27 23:27:14 +0800

    改UI主题色

--- ToAccountOrNot/app/src/main/java/com/example/toaccountornot/SplashActivity.java
+++ ToAccountOrNot/app/src/main/java/com/example/toaccountornot/SplashActivity.java
@@ -6,6 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.example.toaccountornot.utils.Cards;
+
+import org.litepal.LitePal;
+
+import java.util.List;
+
 /**
  * 启动页
  */
@@ -17,6 +23,7 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         initDateBase();
+        initcarddata();
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
@@ -33,6 +40,30 @@ public class SplashActivity extends AppCompatActivity {
      */
     public void initDateBase() {
 
+    }
+
+    private void initcarddata(){
+        List<Cards> wechat = LitePal.where("card = ?","微信").find(Cards.class);
+        if (wechat.size()==0){
+            Cards card = new Cards();
+            card.setCard("微信");
+            card.setImage(R.drawable.wechat);
+            card.save();
+        }
+        List<Cards> alipay  = LitePal.where("card = ?","支付宝").find(Cards.class);
+        if (alipay.size()==0){
+            Cards card = new Cards();
+            card.setCard("支付宝");
+            card.setImage(R.drawable.alipay);
+            card.save();
+        }
+        List<Cards> cash = LitePal.where("card = ?","现金").find(Cards.class);
+        if (cash.size() == 0){
+            Cards card = new Cards();
+            card.setCard("现金");
+            card.setImage(R.drawable.cash);
+            card.save();
+        }
     }
 
 }
