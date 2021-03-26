27bc21dbb63c5192c362470d3e441447010150d4
jyow <49602630+caskr@users.noreply.github.com>
2021-03-26 17:15:36 +0800

    Update LoginActivity.java

--- ToAccountOrNot/app/src/main/java/com/example/toaccountornot/login/LoginActivity.java
+++ ToAccountOrNot/app/src/main/java/com/example/toaccountornot/login/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.example.toaccountornot.login;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -13,9 +15,14 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.toaccountornot.CardDetailActivity;
+import com.example.toaccountornot.NavigationActivity;
 import com.example.toaccountornot.R;
+import com.example.toaccountornot.utils.Accounts;
+import com.example.toaccountornot.utils.Cards;
 
 import org.json.JSONObject;
+import org.litepal.LitePal;
 
 public class LoginActivity extends AppCompatActivity {
     ImageView login_back;
@@ -69,19 +76,53 @@ public class LoginActivity extends AppCompatActivity {
         //获取username & password
         String userName = login_username.getText().toString();
         String password = login_psw.getText().toString();
+        if(userName.isEmpty()){
+            new AlertDialog.Builder(LoginActivity.this)
+                    .setTitle("警告")
+                    .setMessage("用户名不能为空")
+                    .setNegativeButton("返回", null)
+                    .create().show();
+        }else if(password.isEmpty()){
+            new AlertDialog.Builder(LoginActivity.this)
+                    .setTitle("警告")
+                    .setMessage("密码不能为空")
+                    .setNegativeButton("返回", null)
+                    .create().show();
+        }
         //登录逻辑
-        Handler loginhandler = new Handler(Looper.myLooper(), new Handler.Callback() {
-            @Override
-            public boolean handleMessage(@NonNull Message msg) {
-                String response = (msg != null) ? (String)msg.obj : null;
-                if(response != null) {
-//                    try {
-////                        JSONObject jsonObject  = new JSONObject(response);
-////                        String responseCode = jsonObject.getString("responseCode");
-//                    }
-                }
-                return false;
-            }
-        });
+//        Handler loginhandler = new Handler(Looper.myLooper(), new Handler.Callback() {
+//            @Override
+//            public boolean handleMessage(@NonNull Message msg) {
+//                String response = (msg != null) ? (String)msg.obj : null;
+//                if(response != null) {
+////                    try {
+//////                        JSONObject jsonObject  = new JSONObject(response);
+//////                        String responseCode = jsonObject.getString("responseCode");
+////                    }
+//                }
+//                return false;
+//            }
+//        });
+        String correct_psw = "user";//服务器传来正确密码
+        if(correct_psw.equals(password)){
+            onLoginSucceess();
+        }
+        else{
+            onLoginFail();
+        }
+    }
+    void onLoginSucceess() {
+        login_finish.setEnabled(true);
+        Intent intent = new Intent(LoginActivity.this, NavigationActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    void onLoginFail() {
+        login_finish.setEnabled(true);
+        new AlertDialog.Builder(LoginActivity.this)
+                .setTitle("警告")
+                .setMessage("密码错误！请重试")
+                .create().show();
     }
 }
