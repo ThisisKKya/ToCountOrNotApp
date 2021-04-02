a9ebaaa1f7b3ce1cef55ce665d9fbec7ebeb8411
jyow <49602630+caskr@users.noreply.github.com>
2021-04-02 21:24:40 +0800

    Revert "登录按键滑动动画"
    This reverts commit 6cf4c982b83dcbf95d8d32d17600ea7b32d201ae.

--- ToAccountOrNot/app/src/main/java/com/example/toaccountornot/login/LoginActivity.java
+++ ToAccountOrNot/app/src/main/java/com/example/toaccountornot/login/LoginActivity.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.animation.Animator;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,16 +11,13 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.view.View;
-import android.view.ViewAnimationUtils;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.toaccountornot.CardDetailActivity;
 import com.example.toaccountornot.NavigationActivity;
 import com.example.toaccountornot.R;
-import com.example.toaccountornot.button.NbButton;
 import com.example.toaccountornot.utils.Accounts;
 import com.example.toaccountornot.utils.Cards;
 
@@ -33,21 +29,19 @@ public class LoginActivity extends AppCompatActivity {
     EditText login_username;
     EditText login_psw;
     TextView to_sign_up;
-    private NbButton button;
-    private RelativeLayout rlContent;
-    private Handler handler;
-    private Animator animator;
-
+    TextView login_finish;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initView();
+    }
     void initView(){
         login_back = findViewById(R.id.login_back);
         login_username = findViewById(R.id.login_username);
         login_psw = findViewById(R.id.login_psw);
         to_sign_up = findViewById(R.id.to_sign_up);
-        button = findViewById(R.id.login_finish);
-        rlContent=findViewById(R.id.rl_content);
-
-        rlContent.getBackground().setAlpha(0);
-        handler=new Handler();
+        login_finish = findViewById(R.id.login_finish);
 
         login_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,68 +56,13 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        button.setOnClickListener(new View.OnClickListener() {
+        login_finish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 loginIn();
             }
         });
     }
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        initView();
-    }
-
-    private void gotoNew() {
-        button.gotoNew();
-
-        final Intent intent=new Intent(this,NavigationActivity.class);
-
-        int xc=(button.getLeft()+button.getRight())/2;
-        int yc=(button.getTop()+button.getBottom())/2;
-        animator= ViewAnimationUtils.createCircularReveal(rlContent,xc,yc,0,1111);
-        animator.setDuration(200);
-        animator.addListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animation) {
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        startActivity(intent);
-                        overridePendingTransition(R.anim.anim_in,R.anim.anim_out);
-                    }
-                },400);
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-
-            }
-        });
-        animator.start();
-        rlContent.getBackground().setAlpha(255);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        animator.cancel();
-        rlContent.getBackground().setAlpha(0);
-        button.regainBackground();
-    }
-//
 
     @Override
     public void onBackPressed() {
@@ -133,7 +72,7 @@ public class LoginActivity extends AppCompatActivity {
     /*登录逻辑*/
     void loginIn(){
         //防止重复登录
-        button.setEnabled(false);
+        login_finish.setEnabled(false);
         //获取username & password
         String userName = login_username.getText().toString();
         String password = login_psw.getText().toString();
@@ -144,7 +83,7 @@ public class LoginActivity extends AppCompatActivity {
                     .setNegativeButton("返回",new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            button.setEnabled(true);
+                            login_finish.setEnabled(true);
                         }
                     })
                     .create().show();
@@ -155,7 +94,7 @@ public class LoginActivity extends AppCompatActivity {
                     .setNegativeButton("返回", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            button.setEnabled(true);
+                            login_finish.setEnabled(true);
                         }
                     })
                     .create().show();
@@ -185,19 +124,14 @@ public class LoginActivity extends AppCompatActivity {
 
     }
     void onLoginSucceess() {
-        button.setEnabled(true);
-        button.startAnim();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                //跳转
-                gotoNew();
-            }
-        },300);
+        login_finish.setEnabled(true);
+        Intent intent = new Intent(LoginActivity.this, NavigationActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     void onLoginFail() {
-        button.setEnabled(true);
+        login_finish.setEnabled(true);
         new AlertDialog.Builder(LoginActivity.this)
                 .setTitle("警告")
                 .setMessage("密码错误！请重试")
